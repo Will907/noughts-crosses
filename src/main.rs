51689785155ea249cc9f802194879afc42640cae
will <will.pinder.2006@gmail.com>
2024-09-15 22:44:56 +0100

    Added player start selection system

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-//use rand::Rng;
+use rand::Rng;
 use std::io::{self, Write};
 
 fn main() {
     println!("Welcome to noughts and crosses!");
-    let players: u8 = player_select();
-    println!("{players} player game selected");
+    loop {
+        let players: u8 = player_select();
+        game(&players);
+        if players == 1 {
+            break;
+        }
+    }
 }
 
 fn player_select() -> u8 {
@@ -14,7 +19,7 @@ fn player_select() -> u8 {
         if let Err(error) = io::stdout().flush() {
             panic!("{}",error)
         };
-        
+
         io::stdin()
             .read_line(&mut players)
             .expect("Failed to read line");
@@ -31,4 +36,29 @@ fn player_select() -> u8 {
             }
         }
     }
+}
+
+fn game(players: &u8) {
+    println!("{}",players);
+    let mut first: u8 = 0;
+    match rand::thread_rng().gen_range(1..=2) {
+        1 => {
+            first = 1;
+            if players.clone() == 1 {
+                println!("Player goes first")
+            } else {
+                println!("Player 1 goes first")
+            }
+        },
+        2 => {
+            first = 2;
+            if players.clone() == 1 {
+                println!("Computer goes first")
+            } else {
+                println!("Player 2 goes first")
+            }
+        },
+        _ => println!("Not sure what happened here"),
+    };
+    println!("{first}");
 }
